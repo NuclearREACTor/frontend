a42baffb13391a9ea58d96a5fbf7fa44552a8891
manalsolanki <97manal@gmail.com>
2021-10-20 16:14:10 -0400

    Payment Module Setup

--- client/src/OrderPage.js
+++ client/src/OrderPage.js
@@ -4,52 +4,76 @@ import axios from "axios";
 
 function OrderPage(props) {
 
+
     const [receivedData, setReceivedData] = useState({
         isLoaded: false,
-        isDetailLoaded:false,
+        isDetailLoaded: false,
         orderDetails: [],
-        detailedOrders :[]
+
     });
+    const [itemTotal, setItemTotal] = useState(0);
 
     useEffect(() => {
         if (receivedData.isLoaded !== true) {
             getOrder();
         }
     });
+    let finalOrder = {};
+    useEffect(() => {
+        let total = 0;
+        if (receivedData.isLoaded == true) {
+            receivedData.orderDetails.forEach(element => {
+                total = total + (element.quantity * element.price)
+            });
+
+            setItemTotal(total)
+            finalOrder.orderId = props.match.params.id;
+            finalOrder.total = itemTotal;
+        }
+    }, [receivedData.isLoaded]);
+
     const getOrder = () => {
-        axios("http://localhost:8000/order/findbyid/" + props.match.params.id).then((result) => {
+        axios("http://localhost:8000/order/findbyid/" + props.match.params.id).then(async (result) => {
+            for (var a in result.data) {
+                //console.log(result.data[a].food_id);
+                await axios("http://localhost:8000/food/findbyid/" + result.data[a].food_id).then((FoodDetail) => {
+
+                    result.data[a].foodName = FoodDetail.data.foodName;
+                    result.data[a].foodType = FoodDetail.data.foodType;
+                    result.data[a].protein = FoodDetail.data.protein;
+                    result.data[a].calories = FoodDetail.data.calories;
+                    result.data[a].price = FoodDetail.data.price;
+                })
+            }
             setReceivedData({ isLoaded: true, orderDetails: result.data });
-            //   setReceivedDate({ isLoaded: true, items: json.data });
         });
     };
-    // const getFoodDetail = (food_id,quantity) =>{
-    //     axios("http://localhost:8000/order/findbyid/" +food_id).then((result) => {
-    //         setReceivedData({ isDetailLoaded: true, orderDetails: result.data });
-    //         //   setReceivedDate({ isLoaded: true, items: json.data });
-    //     });
-    // }
-    // if(!receivedData.isLoaded)
-    // {
-    //     {receivedData.orderDetails.map(item =>{
-    //         return 
-    //            ( getFoodDetail(item.food_id,item.quantity))
-    //     })}
-    // }
-    if(!receivedData.isLoaded)
-    {   
-        return(
+
+    const payDetails = () => {
+
+        const jsonPayload = finalOrder;
+        axios
+            .post("http://localhost:8000/pay/payNow", {
+                body: jsonPayload,
+            })
+            .then((resp) => {
+                console.log(resp.data);
+            });
+    };
+    if (!receivedData.isLoaded) {
+        return (
             <div>
-                < h1>Loading The Order Details</h1>
+                <h1>Loading The Order Details</h1>
             </div>
         )
     }
-    else 
-    {
+    else {
         return (
             <div>
                 <div className="row justify-content-center">
                     <div className="col-md-6">
                         <h2>Order Details</h2>
+                        { }
                         {/* <div className="p-3">
                             <h2>name: {props.match.params.id}</h2>
                             <p>Price: <b>price</b></p>
@@ -57,29 +81,37 @@ function OrderPage(props) {
                             <p>Total:<b>price * item.quantity $</b></p>
                             <hr />
                         </div> */}
-    
+
                         {receivedData.orderDetails.map(item => {
                             return (
-                                
-                                    <div className="p-3">
-                                        <h2>{item.food_id}</h2>
-                                        <p>Price: <b>{item.quantity}</b></p>
-                                        {/* <p>Quantity:<b>{item.quantity}</b></p>
+
+                                <div className="p-3">
+                                    <h2>{item.foodName}</h2>
+                                    <p>Price: <b>{item.price}</b></p>
+                                    <p>Quantity: <b>{item.quantity}</b></p>
+                                    <p>Sub Total : <b>{item.price * item.quantity}</b></p>
+                                    {/* <p>Quantity:<b>{item.quantity}</b></p>
                                         <p>Total:<b>{item.price * item.quantity} $</b></p> */}
-                                        <hr/>
-                                    </div>
+                                    <hr />
+                                </div>
                             )
                         })}
-    
+
                     </div>
                     <div className="col-md-4">
                         <h2>Total</h2>
-                        <h3>37.5 $</h3>
-                    </div>
+                        <h3>{itemTotal} $</h3>
+                        <button
+                            className="btn btn-success m-2"
+                            type="button"
+                            onClick={payDetails}
+                        >
+                            Pay Now
+                        </button>                    </div>
                 </div>
             </div>
         )
     }
-    
+
 }
 export default OrderPage;
