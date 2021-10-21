858d1804fd673d10867cdef98dc6e14ea8a1f7e5
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-10-21 18:29:23 +0530

    Updated the route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 // import HomePage from "./screens/Homepage";
 import MainPage from "./MainPage";
@@ -11,16 +11,12 @@ import axios from "axios";
 import { AuthContextProvider } from "./context/AuthContext";
 import { useContext } from "react";
 
-
-
-
 import PaymentPage from "./PaymentPage";
 
 // cookies and tokens allowed
 axios.defaults.withCredentials = true;
 
 function App() {
-
   return (
     <div className="App container">
       <AuthContextProvider>
@@ -30,8 +26,6 @@ function App() {
           <Route path="/order/:id" exact component={OrderPage} />
           <Route path="/register" exact component={RegisterPage} />
           <Route path="/login" exact component={LoginPage} />
-          <Route path="/" exact component={MainPage} />
-          <Route path="/order/:id" exact component={OrderPage} />
           <Route path="/payment/:id" exact component={PaymentPage} />
         </BrowserRouter>
       </AuthContextProvider>
