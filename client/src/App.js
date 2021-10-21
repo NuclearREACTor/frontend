bcce7ec3b037c5a332df5c588613b24c90238e4a
ruth frogoso <ruthfrogoso@gmail.com>
2021-10-21 07:02:28 -0400

    added login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter, Route, Link, Switch}from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 // import HomePage from "./screens/Homepage";
 import MainPage from "./MainPage";
 import OrderPage from "./OrderPage";
+import RegisterPage from "./screens/Register";
+import LoginPage from "./screens/LoginPage";
+import axios from "axios";
+import { AuthContextProvider } from "./context/AuthContext";
+import { useContext } from "react";
+
+// cookies and tokens allowed
+axios.defaults.withCredentials = true;
 
 function App() {
+
   return (
     <div className="App container">
+      <AuthContextProvider>
         <Navbar />
-      <BrowserRouter>
-      <Route path="/" exact component ={MainPage}/>
-      <Route path="/order/:id" exact component={OrderPage}/>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/order/:id" exact component={OrderPage} />
+          <Route path="/register" exact component={RegisterPage} />
+          <Route path="/login" exact component={LoginPage} />
+        </BrowserRouter>
+      </AuthContextProvider>
     </div>
   );
 }
