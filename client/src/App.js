71d8d696b2a0275f32b55a93fe4b507b94691cdd
ruth frogoso <ruthfrogoso@gmail.com>
2021-10-21 07:29:31 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 // import HomePage from "./screens/Homepage";
 import MainPage from "./MainPage";
@@ -12,9 +11,13 @@ import axios from "axios";
 import { AuthContextProvider } from "./context/AuthContext";
 import { useContext } from "react";
 
+
+
+
+import PaymentPage from "./PaymentPage";
+
 // cookies and tokens allowed
 axios.defaults.withCredentials = true;
-import PaymentPage from "./PaymentPage";
 
 function App() {
 
@@ -27,14 +30,11 @@ function App() {
           <Route path="/order/:id" exact component={OrderPage} />
           <Route path="/register" exact component={RegisterPage} />
           <Route path="/login" exact component={LoginPage} />
+          <Route path="/" exact component={MainPage} />
+          <Route path="/order/:id" exact component={OrderPage} />
+          <Route path="/payment/:id" exact component={PaymentPage} />
         </BrowserRouter>
       </AuthContextProvider>
-      <Navbar />
-      <BrowserRouter>
-        <Route path="/" exact component={MainPage} />
-        <Route path="/order/:id" exact component={OrderPage} />
-        <Route path="/payment/:id" exact component={PaymentPage} />
-      </BrowserRouter>
     </div>
   );
 }
