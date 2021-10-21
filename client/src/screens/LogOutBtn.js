74d404f58958ffe5c0d315b044e8858870bcfbe5
ruth frogoso <ruthfrogoso@gmail.com>
2021-10-21 07:32:10 -0400

    updated LogOutBtn.js

--- client/src/screens/LogOutBtn.js
+++ client/src/screens/LogOutBtn.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
-import { useHistory } from "react-router-dom";
 
 function LogOutBtn() {
 
   const { getLoggedIn } = useContext(AuthContext);
-  const history = useHistory();
 
   async function logOut() {
     await axios.get("http://localhost:8000/auth/logout");
     await getLoggedIn();
-    history.push("/");
   }
 
   return <button onClick={logOut}>Log out</button>
